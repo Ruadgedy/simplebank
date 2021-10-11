a0abe42328bae1b20097f4d0b3f0b8154f593ebc
Ruadgedy <596318128@qq.com>
2021-10-11 14:21:30 +0800

    Mock DB for testing HTTP API

--- api/account_test.go
+++ api/account_test.go
@@ -1,18 +1,95 @@
 package api
 
 import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/Ruadgedy/simplebank/db/mock"
 	db "github.com/Ruadgedy/simplebank/db/sqlc"
 	"github.com/Ruadgedy/simplebank/util"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestGetAccountApi(t *testing.T)  {
 	account := randomAccount()
 
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	testCases := []struct {
+		name string
+		accountID int64
+		buildStubs func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T,recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(),gomock.Eq(account.ID)).
+					Times(1).
+					Return(account,nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t,http.StatusOK, recorder.Code)
+				requireBodyMatchAccount(t,recorder.Body,account)
+			},
+		},
+		// TODO: add more cases
+		{
+			name: "Not Found",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(db.Account{},sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t,http.StatusNotFound,recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			accountID: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(db.Account{},sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t,http.StatusBadRequest,recorder.Code)
+			},
+		},
+	}
 
+	for i := range testCases{
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/accounts/%d", account.ID)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder,request)
+
+			tc.checkResponse(t,recorder)
+		})
+	}
 
 }
 
@@ -23,4 +100,14 @@ func randomAccount() db.Account {
 		Balance:   util.RandomMoney(),
 		Currency:  util.RandomCurrency(),
 	}
+}
+
+func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer,account db.Account)  {
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t,err)
+
+	var gotAccount db.Account
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+	require.Equal(t,account,gotAccount)
 }
